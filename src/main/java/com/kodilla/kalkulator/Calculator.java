b7cf844886438133a1dc805058c6eb53f21df6c3
Barbara Rutkowska <barbara_rutkowska.22@wp.pl>
2021-09-26 20:26:00 +0200

    Zadanie kalkulator

--- src/main/java/com/kodilla/kalkulator/Calculator.java
+++ src/main/java/com/kodilla/kalkulator/Calculator.java
@@ -1,4 +1,20 @@
 package com.kodilla.kalkulator;
 
 public class Calculator {
+    int a;
+    int b;
+
+    public int add(int a, int b) {
+        return a+b;
+    }
+    public int substract(int a, int b){
+        return a-b;
+    }
+    public static void main(String args[]){
+        Calculator calculator = new Calculator();
+        int add = calculator.add(7, 5);
+        int sub = calculator.substract(7,5);
+        System.out.println(add);
+        System.out.println(sub);
+    }
 }
